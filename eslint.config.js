4c4d413171f1b95405df685c6d389a191aaeb2b0
Leeseojung <sjung0314@gmail.com>
2026-07-28 16:41:14 +0900

    🔧 [Chore/#13] Storybook 및 Chromatic 설정 (#14)
    * 🔧 Chore: Storybook 라이브러리 설치
    
    * 🔧 Chore: Storybook 설정
    
    * 🔧 Chore: chromatic 설정
    
    * ♻️ Refactor: Storybook 전역 설정 적용
    
    * 🔥 Remove: 불필요한 Storybook 및 Chromatic 의존성 제거
    
    * 🐛 Fix: Chromatic PR 댓글 중복 방지
    
    * 🔥 Remove: 사용하지 않는 React import 제거
    
    * 🐛 Fix: Chromatic 포크 PR 실행 방지
    
    * 🔧 Chore: Chromatic 배포 브랜치 확장 및 배포 정보 추가
    
    * 🐛 Fix: Chromatic 배포 일시 형식 수정
    
    * 🔥 Remove: Storybook 테스트 환경 제거
    
    * 🔧 Chore: GitHub Actions SHA 고정

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -7,7 +10,7 @@ import pluginQuery from '@tanstack/eslint-plugin-query'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'storybook-static']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -21,4 +24,5 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  ...storybook.configs["flat/recommended"],
 ])
