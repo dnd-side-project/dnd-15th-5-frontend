9929da1de9f5a867df953cb31900dc28b0c86746
Leeseojeong <sjung0314@gmail.com>
2026-07-29 22:27:48 +0900

    🔧 Chore: ESLint Boundaries 플러그인 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import pluginQuery from '@tanstack/eslint-plugin-query'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import boundaries from 'eslint-plugin-boundaries';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist', 'storybook-static']),
@@ -23,6 +25,66 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    plugins: {
+      boundaries,
+    },
+    settings: {
+      'boundaries/include': ['src/**/*.{ts,tsx}'],
+      'boundaries/elements': [
+        { type: 'app', pattern: 'src/app', partialMatch: false },
+        { type: 'pages', pattern: 'src/pages', partialMatch: false },
+        { type: 'features', pattern: 'src/features', partialMatch: false },
+        { type: 'shared', pattern: 'src/shared', partialMatch: false },
+      ],
+    },
+    rules: {
+      ...boundaries.configs.recommended.rules,
+      'boundaries/dependencies': [
+        'error',
+        {
+          default: 'disallow',
+          policies: [
+            {
+              from: { element: { type: 'app' } },
+              allow: {
+                to: {
+                  element: {
+                    types: { anyOf: ['app', 'pages', 'features', 'shared'] },
+                  },
+                },
+              },
+            },
+            {
+              from: { element: { type: 'pages' } },
+              allow: {
+                to: {
+                  element: {
+                    types: { anyOf: ['pages', 'features', 'shared'] },
+                  },
+                },
+              },
+            },
+            {
+              from: { element: { type: 'features' } },
+              allow: {
+                to: {
+                  element: {
+                    types: { anyOf: ['features', 'shared'] },
+                  },
+                },
+              },
+            },
+            {
+              from: { element: { type: 'shared' } },
+              allow: {
+                to: { element: { type: 'shared' } },
+              },
+            },
+          ],
+        },
+      ],
+    },
   },
-  ...storybook.configs["flat/recommended"],
-])
+  ...storybook.configs['flat/recommended'],
+  eslintConfigPrettier,
+]);
