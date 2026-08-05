2bdd848f3ce649a4517b234c09b009db18565654
Leeseojeong <sjung0314@gmail.com>
2026-08-06 05:57:30 +0900

    ♻️ Refactor: 전체 화면과 패딩 레이아웃 분리

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,10 +2,7 @@ import { GoogleMapView } from '@/features/map';
 
 export default function HomePage() {
   return (
-    <div className="relative h-screen w-full">
-      <h1 className="absolute top-4 left-1/2 z-10 -translate-x-1/2 rounded bg-white px-3 py-2">
-        지도 홈 페이지
-      </h1>
+    <div className="h-screen w-full">
       <GoogleMapView />
     </div>
   );
