28755e5dcc9f23cccd13d5f5e61df3304cf71bb2
Leeseojeong <sjung0314@gmail.com>
2026-08-11 17:19:41 +0900

    🔥 Remove: 목업 마커 제거

--- apps/web/src/features/map/constants.ts
+++ apps/web/src/features/map/constants.ts
@@ -1,7 +1,3 @@
-export const MARKER_COUNT = 50;
-
 export const MAP_DEFAULT_CENTER = { lat: 37.5665, lng: 126.978 };
 
 export const MAP_DEFAULT_ZOOM = 13;
-
-export const MARKER_SPREAD_DEGREE = 0.05;
