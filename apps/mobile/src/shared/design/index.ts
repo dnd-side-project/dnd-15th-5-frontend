c8ff60fb6840d4b891cb5f2a81e9d131b13ba9b2
chae1125 <y72682685@gmail.com>
2026-08-13 05:17:29 +0900

    ♻️ Refactor: 모바일 스타일을 className으로 전환

--- apps/mobile/src/shared/design/index.ts
+++
@@ -1,5 +0,0 @@
-export { COLORS } from './colors';
-export { FONT_FAMILY } from './fonts';
-export { RADIUS } from './radius';
-export { SHADOWS } from './shadows';
-export { TYPOGRAPHY } from './typography';
