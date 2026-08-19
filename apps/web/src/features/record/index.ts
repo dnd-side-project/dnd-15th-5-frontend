41b317a4b4da8881377d542a3a1d3a24ee8228f7
chae1125 <y72682685@gmail.com>
2026-08-19 20:24:44 +0900

    ✨ Feat: 가게 검색 화면에서 수기 입력 폼으로 연결

--- apps/web/src/features/record/index.ts
+++ apps/web/src/features/record/index.ts
@@ -1,1 +1,5 @@
+export { default as ManualRecordForm } from './components/ManualRecordForm';
+export { default as RecordMethodLink } from './components/RecordMethodLink';
 export { useOpenReceiptCamera } from './hooks/useOpenReceiptCamera';
+
+export type { ShopSearchLocationState } from './types';
