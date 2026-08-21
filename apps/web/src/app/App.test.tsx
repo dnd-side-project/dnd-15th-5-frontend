4bddfdc07f683710df02588809cdc84eb03e7c2d
Leeseojeong <sjung0314@gmail.com>
2026-08-21 15:41:11 +0900

    🐛 Fix: 앱 테스트 이미지 로딩 오류 수정

--- apps/web/src/app/App.test.tsx
+++ apps/web/src/app/App.test.tsx
@@ -2,6 +2,11 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('@/shared/assets/images/state', () => ({
+  EmptyStateImage: 'img-empty.png',
+  ErrorStateImage: 'img-error.png',
+}));
+
 describe('App', () => {
   it('정상적으로 렌더링된다', () => {
     const { container } = render(<App />);
