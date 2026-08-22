c51644aaf65e68d91563e0ae0c87573392682522
chae1125 <y72682685@gmail.com>
2026-08-23 04:14:08 +0900

    🔧 Chore: 카테고리별 지도 스티커 이미지 정리

--- apps/web/src/shared/assets/images/stickers/index.ts
+++ apps/web/src/shared/assets/images/stickers/index.ts
@@ -1,21 +1,27 @@
 import CafeCoffeeImage from './img-cafe-coffee.png';
 import CafeDonutImage from './img-cafe-donut.png';
 import CafeIceCreamImage from './img-cafe-ice-cream.png';
+import CommonBravoImage from './img-common-bravo.png';
+import CommonEyesImage from './img-common-eyes.png';
 import EntertainmentDartImage from './img-entertainment-dart.png';
 import EntertainmentLpImage from './img-entertainment-lp.png';
 import EntertainmentMicrophoneImage from './img-entertainment-microphone.png';
 import RestaurantFriesImage from './img-restaurant-fries.png';
 import RestaurantPizzaImage from './img-restaurant-pizza.png';
 import RestaurantSpatulaImage from './img-restaurant-spatula.png';
+import SpecialImage from './img-special.png';
 
 export {
   CafeCoffeeImage,
   CafeDonutImage,
   CafeIceCreamImage,
+  CommonBravoImage,
+  CommonEyesImage,
   EntertainmentDartImage,
   EntertainmentLpImage,
   EntertainmentMicrophoneImage,
   RestaurantFriesImage,
   RestaurantPizzaImage,
   RestaurantSpatulaImage,
+  SpecialImage,
 };
