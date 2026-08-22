049dab521a19142983f54ef35958b7dfa781240f
Leeseojeong <sjung0314@gmail.com>
2026-08-22 12:46:52 +0900

    🔥 Remove: ReportTabs 제거

--- apps/web/src/pages/report/frequent-shops/FrequentShopListPage.tsx
+++ apps/web/src/pages/report/frequent-shops/FrequentShopListPage.tsx
@@ -1,9 +1,6 @@
-import { ReportTabs } from '@/features/report';
-
 export default function FrequentShopListPage() {
   return (
     <main>
-      <ReportTabs active="frequentShops" />
       <p>단골 리스트 페이지</p>
     </main>
   );
