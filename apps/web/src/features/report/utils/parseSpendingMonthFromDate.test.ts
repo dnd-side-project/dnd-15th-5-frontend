24a0a9e358c92acae87ac5cf5df59c7e358857fd
Leeseojeong <sjung0314@gmail.com>
2026-08-22 20:13:41 +0900

    🐛 Fix: 소비내역 날짜 유효성 검증 추가

--- apps/web/src/features/report/utils/parseSpendingMonthFromDate.test.ts
+++ apps/web/src/features/report/utils/parseSpendingMonthFromDate.test.ts
@@ -5,7 +5,21 @@ describe('parseSpendingMonthFromDate', () => {
     expect(parseSpendingMonthFromDate('2026-08-22')).toEqual({ year: 2026, month: 8 });
   });
 
-  it.each([undefined, '', '2026-13-22', '2026-8-22'])('잘못된 날짜는 null을 반환한다', (value) => {
+  it('윤년의 2월 29일을 허용한다', () => {
+    expect(parseSpendingMonthFromDate('2024-02-29')).toEqual({ year: 2024, month: 2 });
+  });
+
+  it.each([
+    undefined,
+    '',
+    '2026-13-22',
+    '2026-8-22',
+    '2026-08-00',
+    '2026-08-32',
+    '2026-02-29',
+    '2026-04-31',
+    '2100-02-29',
+  ])('잘못된 날짜는 null을 반환한다', (value) => {
     expect(parseSpendingMonthFromDate(value)).toBeNull();
   });
 });
