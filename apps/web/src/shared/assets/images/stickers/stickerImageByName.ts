1a1101ea6c6204dad0debb94faf2fb86007fda5b
chae1125 <y72682685@gmail.com>
2026-08-24 03:45:42 +0900

    🎨 Style: 스티커 이미지 에셋 교체 및 매핑 추가

--- apps/web/src/shared/assets/images/stickers/stickerImageByName.ts
+++ apps/web/src/shared/assets/images/stickers/stickerImageByName.ts
@@ -11,19 +11,21 @@ import StickerMicrophoneImage from './img-sticker-microphone.png';
 import StickerPizzaImage from './img-sticker-pizza.png';
 import StickerSpecialImage from './img-sticker-special.png';
 
-export { getStickerImageByName } from './stickerImageByName';
-
-export {
-  StickerBravoImage,
-  StickerCoffeeImage,
-  StickerDartImage,
-  StickerDonutImage,
-  StickerEyesImage,
-  StickerFlipperImage,
-  StickerFriesImage,
-  StickerIceCreamImage,
-  StickerLpImage,
-  StickerMicrophoneImage,
-  StickerPizzaImage,
-  StickerSpecialImage,
+/** 백엔드 스티커 이름을 웹에서 표시할 이미지로 변환합니다. */
+const STICKER_IMAGE_BY_NAME: Record<string, string> = {
+  커피: StickerCoffeeImage,
+  도넛: StickerDonutImage,
+  아이스크림: StickerIceCreamImage,
+  다트: StickerDartImage,
+  LP: StickerLpImage,
+  마이크: StickerMicrophoneImage,
+  감자튀김: StickerFriesImage,
+  피자: StickerPizzaImage,
+  뒤집개: StickerFlipperImage,
+  브라보: StickerBravoImage,
+  눈: StickerEyesImage,
+  스페셜: StickerSpecialImage,
 };
+
+export const getStickerImageByName = (stickerName?: string) =>
+  STICKER_IMAGE_BY_NAME[stickerName ?? ''] ?? StickerSpecialImage;
